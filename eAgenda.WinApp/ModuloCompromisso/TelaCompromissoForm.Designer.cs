7209f627820feb51d08ac3d80a88e24be0ce2447
Fabiano Silva Lima <fabiano_slima@hotmail.com>
2024-05-21 16:38:06 -0300

    implementado layout compromisso

---
+++ eAgenda.WinApp/ModuloCompromisso/TelaCompromissoForm.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace eAgenda.WinApp.ModuloCompromisso
+{
+    partial class TelaCompromissoForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            textBox1 = new TextBox();
+            textBox3 = new TextBox();
+            label3 = new Label();
+            groupBox1 = new GroupBox();
+            textBox5 = new TextBox();
+            textBox4 = new TextBox();
+            radioButton2 = new RadioButton();
+            radioButton1 = new RadioButton();
+            label2 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            checkBox1 = new CheckBox();
+            label7 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            dateTimePicker1 = new DateTimePicker();
+            dateTimePicker2 = new DateTimePicker();
+            dateTimePicker3 = new DateTimePicker();
+            comboBox1 = new ComboBox();
+            groupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Font = new Font("Segoe UI", 11.25F);
+            label1.Location = new Point(52, 59);
+            label1.Name = "label1";
+            label1.Size = new Size(64, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Assunto:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(122, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(269, 23);
+            textBox1.TabIndex = 1;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(122, 27);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(93, 23);
+            textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Segoe UI", 11.25F);
+            label3.Location = new Point(91, 27);
+            label3.Name = "label3";
+            label3.Size = new Size(25, 20);
+            label3.TabIndex = 4;
+            label3.Text = "Id:";
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(textBox5);
+            groupBox1.Controls.Add(textBox4);
+            groupBox1.Controls.Add(radioButton2);
+            groupBox1.Controls.Add(radioButton1);
+            groupBox1.Font = new Font("Segoe UI", 11.25F);
+            groupBox1.Location = new Point(52, 177);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(509, 138);
+            groupBox1.TabIndex = 6;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Local";
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(115, 70);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(384, 27);
+            textBox5.TabIndex = 3;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(115, 28);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(384, 27);
+            textBox4.TabIndex = 2;
+            // 
+            // radioButton2
+            // 
+            radioButton2.AutoSize = true;
+            radioButton2.Font = new Font("Segoe UI", 11.25F);
+            radioButton2.Location = new Point(15, 73);
+            radioButton2.Name = "radioButton2";
+            radioButton2.Size = new Size(70, 24);
+            radioButton2.TabIndex = 1;
+            radioButton2.TabStop = true;
+            radioButton2.Text = "Online";
+            radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton1
+            // 
+            radioButton1.AutoSize = true;
+            radioButton1.Font = new Font("Segoe UI", 11.25F);
+            radioButton1.Location = new Point(16, 31);
+            radioButton1.Name = "radioButton1";
+            radioButton1.Size = new Size(93, 24);
+            radioButton1.TabIndex = 0;
+            radioButton1.TabStop = true;
+            radioButton1.Text = "Presencial";
+            radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Segoe UI", 11.25F);
+            label2.Location = new Point(68, 85);
+            label2.Name = "label2";
+            label2.Size = new Size(44, 20);
+            label2.TabIndex = 7;
+            label2.Text = "Data:";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Segoe UI", 11.25F);
+            label4.Location = new Point(57, 119);
+            label4.Name = "label4";
+            label4.Size = new Size(103, 20);
+            label4.TabIndex = 8;
+            label4.Text = "Horário Início:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Segoe UI", 11.25F);
+            label5.Location = new Point(332, 114);
+            label5.Name = "label5";
+            label5.Size = new Size(98, 20);
+            label5.TabIndex = 9;
+            label5.Text = "Horário Final:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Segoe UI", 11.25F);
+            label6.Location = new Point(57, 158);
+            label6.Name = "label6";
+            label6.Size = new Size(214, 20);
+            label6.TabIndex = 10;
+            label6.Text = "Deseja selecionar um Contato?";
+            // 
+            // checkBox1
+            // 
+            checkBox1.AutoSize = true;
+            checkBox1.Location = new Point(277, 164);
+            checkBox1.Name = "checkBox1";
+            checkBox1.Size = new Size(15, 14);
+            checkBox1.TabIndex = 11;
+            checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Segoe UI", 11.25F);
+            label7.Location = new Point(340, 156);
+            label7.Name = "label7";
+            label7.Size = new Size(65, 20);
+            label7.TabIndex = 12;
+            label7.Text = "Contato:";
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 11.25F);
+            button1.Location = new Point(330, 332);
+            button1.Name = "button1";
+            button1.Size = new Size(100, 37);
+            button1.TabIndex = 13;
+            button1.Text = "Gravar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Font = new Font("Segoe UI", 11.25F);
+            button2.Location = new Point(436, 332);
+            button2.Name = "button2";
+            button2.Size = new Size(100, 37);
+            button2.TabIndex = 14;
+            button2.Text = "Cancelar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // dateTimePicker1
+            // 
+            dateTimePicker1.CustomFormat = "";
+            dateTimePicker1.Format = DateTimePickerFormat.Custom;
+            dateTimePicker1.Location = new Point(122, 83);
+            dateTimePicker1.Name = "dateTimePicker1";
+            dateTimePicker1.Size = new Size(97, 23);
+            dateTimePicker1.TabIndex = 16;
+            // 
+            // dateTimePicker2
+            // 
+            dateTimePicker2.CustomFormat = "hh:mm";
+            dateTimePicker2.Format = DateTimePickerFormat.Custom;
+            dateTimePicker2.Location = new Point(166, 116);
+            dateTimePicker2.Name = "dateTimePicker2";
+            dateTimePicker2.Size = new Size(105, 23);
+            dateTimePicker2.TabIndex = 17;
+            // 
+            // dateTimePicker3
+            // 
+            dateTimePicker3.CustomFormat = "hh:mm";
+            dateTimePicker3.Format = DateTimePickerFormat.Custom;
+            dateTimePicker3.Location = new Point(436, 112);
+            dateTimePicker3.Name = "dateTimePicker3";
+            dateTimePicker3.Size = new Size(105, 23);
+            dateTimePicker3.TabIndex = 18;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "a", "b", "c", "d" });
+            comboBox1.Location = new Point(411, 153);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(138, 23);
+            comboBox1.TabIndex = 19;
+            // 
+            // TelaCompromissoForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(573, 381);
+            Controls.Add(comboBox1);
+            Controls.Add(dateTimePicker3);
+            Controls.Add(dateTimePicker2);
+            Controls.Add(dateTimePicker1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label7);
+            Controls.Add(checkBox1);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label2);
+            Controls.Add(groupBox1);
+            Controls.Add(textBox3);
+            Controls.Add(label3);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            MaximizeBox = false;
+            MinimizeBox = false;
+            Name = "TelaCompromissoForm";
+            Text = "Cadastro de Compromissos";
+            Load += TelaCompromissoForm_Load;
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private TextBox textBox1;
+        private TextBox textBox3;
+        private Label label3;
+        private GroupBox groupBox1;
+        private TextBox textBox5;
+        private TextBox textBox4;
+        private RadioButton radioButton2;
+        private RadioButton radioButton1;
+        private Label label2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private CheckBox checkBox1;
+        private Label label7;
+        private Button button1;
+        private Button button2;
+        private DateTimePicker dateTimePicker1;
+        private DateTimePicker dateTimePicker2;
+        private DateTimePicker dateTimePicker3;
+        private ComboBox comboBox1;
+    }
+}
